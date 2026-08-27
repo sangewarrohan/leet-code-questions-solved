9c05d528c5851699b5eb0c92ba642a9a0fa2f5bd
Sangewar Rohan <236167961+sangewarrohan@users.noreply.github.com>
2026-08-27 09:55:20 +0530

    CodePusher: Solution for 151. Reverse Words in a String

--- Two Pointers/151. Reverse Words in a String/151. Reverse Words in a String.py
+++ Two Pointers/151. Reverse Words in a String/151. Reverse Words in a String.py
@@ -1,9 +1,10 @@
 # Problem: 151. Reverse Words in a String
 # Runtime: 0 ms (Beats 100.00%)
-# Memory: 19.4 MB (Beats 10.93%)
+# Memory: 12.6 MB (Beats 36.53%)
 
-class Solution:
-    def reverseWords(self, s: str) -> str:
+class Solution(object):
+    def reverseWords(self, s):
         words = s.split()
         words.reverse()
         return " ".join(words)
+        
