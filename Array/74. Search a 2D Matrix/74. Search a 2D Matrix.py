d7a319221914373e822d4e9b57809c1429044231
Sangewar Rohan <236167961+sangewarrohan@users.noreply.github.com>
2026-08-27 15:11:05 +0530

    CodePusher: Solution for 74. Search a 2D Matrix

--- Array/74. Search a 2D Matrix/74. Search a 2D Matrix.py
+++ Array/74. Search a 2D Matrix/74. Search a 2D Matrix.py
@@ -1,11 +1,12 @@
 # Problem: 74. Search a 2D Matrix
 # Runtime: 0 ms (Beats 100.00%)
-# Memory: 12.7 MB (Beats 32.39%)
+# Memory: 12.7 MB (Beats 2.35%)
 
 class Solution(object):
     def searchMatrix(self, matrix, target):
         m = len(matrix)
         n = len(matrix[0])
+
         left = 0
         right = m * n - 1
         while left <= right:
@@ -18,4 +19,46 @@ class Solution(object):
                 left = mid + 1
             else:
                 right = mid - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
         return False
