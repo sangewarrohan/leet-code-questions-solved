f2aa2afec65c5098c8a5f70e6f99c1b86c626949
Sangewar Rohan <236167961+sangewarrohan@users.noreply.github.com>
2026-08-27 09:52:19 +0530

    CodePusher: Solution for 1. Two Sum

--- Array/1. Two Sum/1. Two Sum.py
+++ Array/1. Two Sum/1. Two Sum.py
@@ -1,8 +1,8 @@
 # Problem: 1. Two Sum
-# Runtime: 0 ms (Beats 100.00%)
-# Memory: 12.9 MB (Beats 82.39%)
+# Runtime: 3 ms (Beats 54.31%)
+# Memory: 13 MB (Beats 82.39%)
 
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
         hashmap = {}
 
